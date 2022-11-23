7fd9c27e99acb61910b4a8d5570ce6f9a16ce48a
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-11-23 11:54:13 +0300

    WinFormsApp3

--- WinFormsApp3/WinFormsApp3/Form1.cs
+++ WinFormsApp3/WinFormsApp3/Form1.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using static System.Windows.Forms.LinkLabel;
 
 namespace WinFormsApp3
 {
@@ -110,13 +111,13 @@ namespace WinFormsApp3
 
         private void saveToolStripButton_Click(object sender, EventArgs e)
         {
-            // subject|detail|23.12.2022 23:00|true
+            // subject|detail|23.12.2022 23:00|true|ADE567-ASS1234-2323...
 
             List<string> lines = new List<string>(todos.Count);
 
             foreach (Todo item in todos)
             {
-                string line = $"{item.Subject}|{item.Detail}|{item.DueDate}|{item.IsCompleted}";
+                string line = $"{item.Subject}|{item.Detail}|{item.DueDate}|{item.IsCompleted}|{item.Id}";
                 lines.Add(line);
             }
 
@@ -136,6 +137,41 @@ namespace WinFormsApp3
             {
                 File.WriteAllLines(saveFileDialog.FileName, lines);
                 MessageBox.Show("Veriler kaydedildi.", "Kaydetme Yapýldý");
+            }
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();   // Kullanýcýya daha önce kaydedilen dosya seçtirilir.
+            openFileDialog.Title = "Verileri Aç";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "Metin Belgeleri(*.txt)|*.txt|Todo App Format(*.todo)|*.todo";
+
+            DialogResult result = openFileDialog.ShowDialog();  // Open FileDialog açýlýr.
+
+            if (result == DialogResult.OK)                  // Eðer dosya seçip Tamam düðmesine bastý ise;
+            {
+                string path = openFileDialog.FileName;      // Dosya konumu alýnýr.
+                string[] lines = File.ReadAllLines(path);   // Dosyadan tüm satýrlar string olarak okunur.
+
+                todos.Clear();                              // Elimizdeki liste temizlenir.
+
+                foreach (string line in lines)              // Tüm okunan satýrlar için dönülür.
+                {
+                    string[] splitted = line.Split("|");    // Satýr verisi parçalanýr.
+                                                            // subject|detail|23.12.2022 23:00|true
+                    Todo todo = new();                      // Todo nesnesi oluþturulur.
+                    todo.Subject = splitted[0];
+                    todo.Detail = splitted[1];
+                    todo.DueDate = DateTime.Parse(splitted[2]);
+                    todo.IsCompleted = bool.Parse(splitted[3]);
+                    todo.Id = Guid.Parse(splitted[4]);
+
+                    todos.Add(todo);                        // Listeye oluþturulan Todo nesnesi eklenir.
+                }
+
+                LoadListboxMethod2();                       // liste den Listbox doldurulur.
             }
         }
     }
