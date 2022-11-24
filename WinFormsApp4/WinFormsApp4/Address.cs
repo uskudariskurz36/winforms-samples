ac267a196b6a4783cd9e5f46c60c8e916136f552
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-11-24 09:44:52 +0300

    WinFormsApp4

---
+++ WinFormsApp4/WinFormsApp4/Address.cs
@@ -0,0 +1,45 @@
+﻿namespace WinFormsApp4
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public Country Country { get; set; }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public string RegionCode { get; set; }
+    }
+
+    public class Experience
+    {
+        public string Company { get; set; }
+        public ExperienceAddress Address { get; set; }
+
+    }
+
+    public class ExperienceAddress
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Detail { get; set; }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string TcNo { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Male { get; set; }
+        public List<Address> Addresses { get; set; }
+        public List<Experience> Experiences { get; set; }
+    }
+
+}
---
+++ WinFormsApp4/WinFormsApp4/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.txtTcNo = new System.Windows.Forms.MaskedTextBox();
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDistrict = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtCountryName = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPostalCode = new System.Windows.Forms.MaskedTextBox();
+            this.txtCountryRegionCode = new System.Windows.Forms.MaskedTextBox();
+            this.btnAddAddress = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreateUser);
+            this.groupBox1.Controls.Add(this.txtTcNo);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.dtpBirthdate);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 510);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(16, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(247, 23);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Surname";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(16, 95);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(247, 23);
+            this.txtSurname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "TC No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Phone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 227);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(247, 23);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 253);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Birthdate";
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(16, 271);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(247, 23);
+            this.dtpBirthdate.TabIndex = 2;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(16, 183);
+            this.txtPhone.Mask = "(999) 000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(247, 23);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(16, 139);
+            this.txtTcNo.Mask = "00000000000";
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(247, 23);
+            this.txtTcNo.TabIndex = 3;
+            this.txtTcNo.ValidatingType = typeof(int);
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.Location = new System.Drawing.Point(188, 300);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateUser.TabIndex = 4;
+            this.btnCreateUser.Text = "Create";
+            this.btnCreateUser.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.btnAddAddress);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.txtPostalCode);
+            this.groupBox2.Controls.Add(this.txtCity);
+            this.groupBox2.Controls.Add(this.txtDistrict);
+            this.groupBox2.Controls.Add(this.txtStreet);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(298, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 510);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "User Addresses";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Street";
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(6, 193);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(236, 23);
+            this.txtStreet.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 15);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "District";
+            // 
+            // txtDistrict
+            // 
+            this.txtDistrict.Location = new System.Drawing.Point(6, 237);
+            this.txtDistrict.Name = "txtDistrict";
+            this.txtDistrict.Size = new System.Drawing.Size(236, 23);
+            this.txtDistrict.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "City";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(6, 281);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(236, 23);
+            this.txtCity.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 307);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 15);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Postal Code";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.txtCountryRegionCode);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.txtCountryName);
+            this.groupBox3.Location = new System.Drawing.Point(6, 354);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(236, 121);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Address Country";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 15);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Name";
+            // 
+            // txtCountryName
+            // 
+            this.txtCountryName.Location = new System.Drawing.Point(6, 41);
+            this.txtCountryName.Name = "txtCountryName";
+            this.txtCountryName.Size = new System.Drawing.Size(224, 23);
+            this.txtCountryName.TabIndex = 1;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 67);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 15);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Region Code";
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(6, 325);
+            this.txtPostalCode.Mask = "000000";
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(236, 23);
+            this.txtPostalCode.TabIndex = 3;
+            this.txtPostalCode.ValidatingType = typeof(int);
+            // 
+            // txtCountryRegionCode
+            // 
+            this.txtCountryRegionCode.Location = new System.Drawing.Point(6, 89);
+            this.txtCountryRegionCode.Mask = "+000";
+            this.txtCountryRegionCode.Name = "txtCountryRegionCode";
+            this.txtCountryRegionCode.Size = new System.Drawing.Size(224, 23);
+            this.txtCountryRegionCode.TabIndex = 3;
+            this.txtCountryRegionCode.ValidatingType = typeof(int);
+            // 
+            // btnAddAddress
+            // 
+            this.btnAddAddress.Location = new System.Drawing.Point(167, 481);
+            this.btnAddAddress.Name = "btnAddAddress";
+            this.btnAddAddress.Size = new System.Drawing.Size(75, 23);
+            this.btnAddAddress.TabIndex = 4;
+            this.btnAddAddress.Text = "Add";
+            this.btnAddAddress.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(6, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 139);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 534);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private MaskedTextBox txtTcNo;
+        private MaskedTextBox txtPhone;
+        private DateTimePicker dtpBirthdate;
+        private TextBox txtEmail;
+        private TextBox txtSurname;
+        private TextBox txtName;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button btnCreateUser;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private Label label12;
+        private Label label11;
+        private TextBox txtCountryName;
+        private TextBox txtCity;
+        private TextBox txtDistrict;
+        private TextBox txtStreet;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private ListBox listBox1;
+        private Button btnAddAddress;
+        private MaskedTextBox txtCountryRegionCode;
+        private MaskedTextBox txtPostalCode;
+    }
+}
