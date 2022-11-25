61ce4d2173d6603ebbdc76b2b6c3f8bdb1be548c
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-11-25 13:26:07 +0300

    WinFormsApp6_AracKiralama

---
+++ WinFormsApp6_AracKiralama/WinFormsApp6_AracKiralama/frmModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6_AracKiralama
+{
+    public partial class frmModels : Form
+    {
+        public frmModels()
+        {
+            InitializeComponent();
+        }
+    }
+}
